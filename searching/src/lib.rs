189dee792e77e75eb5ac9082b77280de21bdfd6a
garveyshah <garveyshah1595@gmail.com>
2025-04-12 05:58:04 +0300

    feat: Add initial implementation of search function and main entry point

---
+++ searching/src/lib.rs
@@ -0,0 +1,9 @@
+pub fn search(array: &[i32], key: i32) -> Option<usize> {
+
+    for (i, value) in array.iter().enumerate() {
+        if value == &key {
+            return Some(i) 
+        }
+    }
+    None
+}

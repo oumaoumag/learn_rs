8fa46a4c814862bb8fb5aa823e806fd0c21badd5
garveyshah <garveyshah1595@gmail.com>
2025-04-12 11:17:06 +0300

    feat: Add initial implementation of rgb_match package with Color struct and swap method

---
+++ rgb_match/src/lib.rs
@@ -0,0 +1,32 @@
+use std::mem::swap;
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct Color {
+    pub r: u8,
+    pub g: u8,
+    pub b: u8,
+    pub a: u8,
+}
+
+impl Color {
+    pub fn swap(mut self, first: u8, second: u8) -> Color {
+        match (first, second) {
+            (f, s) if f == self.r && s == self.g => { std::mem::swap(&mut self.r, &mut self.g); }
+            (f, s) if f == self.r && s == self.b => { std::mem::swap(&mut self.r, &mut self.b); }
+            (f, s) if f == self.r && s == self.a => { std::mem::swap(&mut self.r, &mut self.a); }
+            (f, s) if f == self.g && s == self.r => { std::mem::swap(&mut self.g, &mut self.r); }
+
+            (f, s) if f == self.g && s == self.b => { std::mem::swap(&mut self.g, &mut self.b); }
+            (f, s) if f == self.g && s == self.a => { std::mem::swap(&mut self.g, &mut self.a); }
+            (f, s) if f == self.b && s == self.r => { std::mem::swap(&mut self.b, &mut self.r); }
+            (f, s) if f == self.b && s == self.g => { std::mem::swap(&mut self.b, &mut self.g); }
+
+            (f, s) if f == self.b && s == self.a => { std::mem::swap(&mut self.b, &mut self.a); }
+            (f, s) if f == self.a && s == self.r => { std::mem::swap(&mut self.a, &mut self.r); }
+            (f, s) if f == self.a && s == self.g => { std::mem::swap(&mut self.a, &mut self.g); }
+            (f, s) if f == self.a && s == self.b => { std::mem::swap(&mut self.a, &mut self.b); }
+            _=> {}
+        }
+        self
+    }
+}

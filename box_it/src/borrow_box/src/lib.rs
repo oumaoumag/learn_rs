73c6aaf505aad779a215ae1de729f957f7673b0a
oumaoumag <ouma.godwin10@gmail.com>
2025-04-17 23:35:23 +0300

    fix: Correct typos in GameSession struct initialization and read_winner method

--- box_it/src/borrow_box/src/lib.rs
+++ box_it/src/borrow_box/src/lib.rs
@@ -9,7 +9,7 @@ pub struct GameSession {
 impl GameSession {
     // Initializes a new game session with player names
     pub fn new(id: u32, p1_name: String, p2_name: String, nb_games: u16) -> Box<GameSession> {
-        Box::new(GmaeSession {
+        Box::new(GameSession {
             id, 
             p1: (p1_name, 0),
             p2: (p2_name, 0),
@@ -20,7 +20,7 @@ impl GameSession {
     // Returns the name and score of the winning player or a tie message
     pub fn read_winner(&self) -> (String, u16) {
         if self.p1.1 == self.p2.1 {
-            ("Same score! tied".to_String(). self.p1.1)
+            ("Same score! tied".to_string(), self.p1.1)
         } else if self.p1.1 > self.p1.1 {
             (self.p1.0.clone(), self.p1.1)
         } else {

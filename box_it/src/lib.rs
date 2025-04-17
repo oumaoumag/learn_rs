5d335b35064ecf9e020e051c227366a503aeaa9c
oumaoumag <ouma.godwin10@gmail.com>
2025-04-17 22:58:19 +0300

    feat: Implement transform_and_save_on_heap and take_value_ownership functions with main example

--- box_it/src/lib.rs
+++ box_it/src/lib.rs
@@ -1,14 +1,25 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+pub fn transform_and_save_on_heap(s: String) -> Box<Vec<u32>> {
+    let mut result = Vec::new();
+
+    for num_str in s.split_whitespace() {
+        if num_str.ends_with('k') {
+            let num_without_k = &num_str[0..num_str.len() -1];
+            if let Ok(num) = num_without_k.parse::<f32>() {
+                result.push((num * 1000.0) as u32);
+            }
+        } else {
+            // Parsee directly as f32 and convert to u32
+            if let Ok(num) = num_str.parse::<f32>() {
+                result.push(num as u32);
+            }
+        }
+    }
+
+    // Box the vector and return it
+    Box::new(result)
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub fn take_value_ownership(a: Box<Vec<u32>>) -> Vec<u32> {
+    // Unboxing the vector by dereferencing the Box
+    *a
 }

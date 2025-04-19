d5755cdff1e822c3917fe83408739c71895cae48
oumaoumag <ouma.godwin10@gmail.com>
2025-04-19 23:28:57 +0300

    chore: Add Cargo.toml for project configuration and update comments in lib.rs for clarity

--- generics_list/src/lib.rs
+++ generics_list/src/lib.rs
@@ -10,36 +10,33 @@ pub struct Node<T> {
 }
 
 impl<T> List<T> {
-    // creats an empty list
     pub fn new() -> List<T> {
         List { head: None }
     }
 
-    // Adds an element to the beginning of the list
     pub fn push(&mut self, value: T) {
         let new_node = Box::new(Node {
             value,
-            next: self.head.take(), 
+            next: self.head.take(),
         });
         self.head = Some(new_node);
     }
 
-    // removes te first element from the list
     pub fn pop(&mut self) {
-        if let Some(mut boxed_node) = self.head.take() {
-            self.head = boxed_node.next.take();
+        if let Some(boxed_node) = self.head.take() {
+            self.head = boxed_node.next;
         }
     }
 
-    // Counts the number of elements in the list
     pub fn len(&self) -> usize {
         let mut count = 0;
         let mut current = &self.head;
-
-        while let Some(node) = current {
+        
+        while let Some(_) = current {
             count += 1;
-            current = &node.next;
+            current = &current.as_ref().unwrap().next;
         }
+        
         count
     }
 }

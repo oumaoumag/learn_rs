1687c8f5a99bd047a5540c15aa9d0f3f14b38c68
oumaoumag <ouma.godwin10@gmail.com>
2025-04-19 23:46:41 +0300

    refactor: Change List head type from Option<Box<Node<T>>> to Option<Node<T>> for improved memory management

--- generics_list/src/lib.rs
+++ generics_list/src/lib.rs
@@ -1,6 +1,6 @@
 #[derive(Clone, Debug)]
 pub struct List<T> {
-    pub head: Option<Box<Node<T>>>, 
+    pub head: Option<Node<T>>,
 }
 
 #[derive(Clone, Debug)]
@@ -15,26 +15,26 @@ impl<T> List<T> {
     }
 
     pub fn push(&mut self, value: T) {
-        let new_node = Box::new(Node {
+        let new_node = Node {
             value,
-            next: self.head.take(),
-        });
+            next: self.head.take().map(Box::new),
+        };
         self.head = Some(new_node);
     }
 
     pub fn pop(&mut self) {
-        if let Some(boxed_node) = self.head.take() {
-            self.head = boxed_node.next;
+        if let Some(node) = self.head.take() {
+            self.head = node.next.map(|boxed_node| *boxed_node);
         }
     }
 
     pub fn len(&self) -> usize {
         let mut count = 0;
-        let mut current = &self.head;
+        let mut current = self.head.as_ref();
         
-        while let Some(_) = current {
+        while let Some(node) = current {
             count += 1;
-            current = &current.as_ref().unwrap().next;
+            current = node.next.as_ref().map(|boxed| &**boxed);
         }
         
         count

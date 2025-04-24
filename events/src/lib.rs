66ac67ae7400ee2ba90948492e5190b9761bb8fa
oumoumag <ouma.godwin10@gmail.com>
2025-04-25 01:48:36 +0300

    fix: Correct formatting and typo in notify function for Event enum

--- events/src/lib.rs
+++ events/src/lib.rs
@@ -35,44 +35,42 @@ impl fmt::Display for Notification {
 
 impl<'a> Event<'a> {
     pub fn notify(&self) -> Notification {
-       match self {
+        match self {
             Event::Remainder(text) => Notification {
                 size: 50,
                 color: (50, 50, 50),
                 position: Position::Bottom,
                 content: text.to_string(),
             },
-
             Event::Registration(duration) => {
-               let hours =  duration.num_hours();
+                let hours = duration.num_hours();
                 let minutes = duration.num_minutes() % 60;
                 let seconds = duration.num_seconds() % 60;
-            
-            Notification {
-                size: 30,
-                color: (255, 2, 22),
-                position: Position::Top,
-                content: format!("You have {}H:{}M: {}5 left before the registration ends", hours, minutes, seconds)
-               }
-            },
-
+                Notification {
+                    size: 30,
+                    color: (255, 2, 22),
+                    position: Position::Top,
+                    content: format!(
+                        "You have {}H:{}M:{}S left before the registration ends",
+                        hours, minutes, seconds
+                    ),
+                }
+            }
             Event::Appointment(text) => Notification {
                 size: 100,
                 color: (200, 200, 3),
                 position: Position::Center,
-                content: text.to_string(),               
+                content: text.to_string(),
             },
-
             Event::Holiday => Notification {
                 size: 25,
                 color: (0, 255, 0),
                 position: Position::Top,
-                content: "Enyoy your holiday".to_string(),
+                content: "Enjoy your holiday".to_string(),
             },
-        } 
+        }
     }
 }
-
 
 #[cfg(test)]
 mod tests {

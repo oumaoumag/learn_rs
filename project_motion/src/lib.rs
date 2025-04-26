f928cff9b1bb265583dace852550e26a4090ecf0
oumoumag <ouma.godwin10@gmail.com>
2025-04-27 00:15:56 +0300

    refactor: Remove unused add function and associated tests

--- project_motion/src/lib.rs
+++ project_motion/src/lib.rs
@@ -1,17 +1,3 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
 #[derive(Debug, Clone, PartialEq)]
 pub struct Object {
     pub x: f32,

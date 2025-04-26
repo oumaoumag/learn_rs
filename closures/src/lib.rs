8911a7f4531236b6a42fb807d740f541afb8ecde
oumoumag <ouma.godwin10@gmail.com>
2025-04-26 22:37:55 +0300

    fix: Correct filter closure syntax in first_fifty_even_square function

--- closures/src/lib.rs
+++ closures/src/lib.rs
@@ -1,16 +1,4 @@
 pub fn first_fifty_even_square() -> Vec<i32> {
     (0..)
-    .filter(|n| n % 2 == 0).take(50).map(|n| n * n).collect()
+    .filter(|&n| n % 2 == 0).take(50).map(|n| n * n).collect()
 }
-
-
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-
-//     #[test]
-//     fn it_works() {
-//         let result = add(2, 2);
-//         assert_eq!(result, 4);
-//     }
-// }

dd23300e403c9911cfa6601e96373a8e9802864c
oumoumag <ouma.godwin10@gmail.com>
2025-04-26 22:44:48 +0300

    feat: Make first_fifty_even_square function public

--- closures/src/lib.rs
+++ closures/src/lib.rs
@@ -1,5 +1,6 @@
-fn first_fifty_even_square() -> Vec<i32> {
-    (0..).step_by(2)
+pub fn first_fifty_even_square() -> Vec<i32> {
+    (0..)
+        .step_by(2)
         .take(50)
         .map(|n| n * n)
         .collect()

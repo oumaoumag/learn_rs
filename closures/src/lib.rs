4053eb92566a7c4392a13fa7a53f8efbdd616ff4
oumoumag <ouma.godwin10@gmail.com>
2025-04-26 22:40:04 +0300

    refactor: Improve readability of first_fifty_even_square function by formatting and restructuring code

--- closures/src/lib.rs
+++ closures/src/lib.rs
@@ -1,4 +1,8 @@
-pub fn first_fifty_even_square() -> Vec<i32> {
+fn first_fifty_even_square() -> Vec<i32> {
+    // Start with 0 as the first even number
     (0..)
-    .filter(|&n| n % 2 == 0).take(50).map(|n| n * n).collect()
+        .filter(|&n| n % 2 == 0)
+        .take(50)
+        .map(|n| n * n)
+        .collect()
 }

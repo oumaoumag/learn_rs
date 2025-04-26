57b363cf23d8290dc171ed1eb99c1c4a66d7d045
oumoumag <ouma.godwin10@gmail.com>
2025-04-26 22:58:00 +0300

    feat: Add Store and Cart structs with methods for managing products and generating receipts

---
+++ sales/src/lib.rs
@@ -0,0 +1,30 @@
+#[derive(Debug, Clone, PartialEq)]
+pub struct Store {
+    pub products: Vec<(String, f32)>,
+}
+impl Store {
+    pub fn new(products: Vec<(String, f32)>) -> Store {
+        Store { products }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Cart {
+    // expected public fields
+}
+impl Cart {
+    pub fn new() -> Cart {}
+    pub fn insert_item(&mut self, s: &Store, ele: String) {}
+    pub fn generate_receipt(&mut self) -> Vec<f32> {}
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}

7aaa12745ec1b09e92833e9dd355a14c76cfa119
oumouma <garveyshah1595@gmail.com>
2025-07-08 15:23:17 +0300

    Refactor search function to store the last index of the key instead of returning immediately

--- searching/src/lib.rs
+++ searching/src/lib.rs
@@ -1,9 +1,9 @@
 pub fn search(array: &[i32], key: i32) -> Option<usize> {
-
+    let mut last_index = None;
     for (i, value) in array.iter().enumerate() {
         if value == &key {
-            return Some(i) 
+            last_index = Some(i);
         }
     }
-    None
+    last_index
 }
